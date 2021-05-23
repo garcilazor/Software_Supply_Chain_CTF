f1c8c55ed7736ab7c3ac1a86bc33a4a051d8db2e
jake-bickle <bickle.jake@gmail.com>
2021-05-23 09:02:00 -0700

    Provide download link for the uploader script

--- src/compromise_dev_workflow/webserver/app.py
+++ src/compromise_dev_workflow/webserver/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/uploader")
+def uploader_download():
+    return send_file("static/uploader")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=80)
