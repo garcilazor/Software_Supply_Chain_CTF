4d93a34fba49ac60ded56a80f249a804b49661dc
Huy Doan <huy26@pdx.edu>
2021-06-14 12:49:21 +0700

    Create main script

---
+++ abandoned-dependencies-ctf/main.py
@@ -0,0 +1,8 @@
+from password.password import getpass
+if __name__ ==  "__main__":
+    name = input ("Full name: ")
+    dob = input ("Date of birth: ")
+    password = getpass("Passowrd: ")
+    re_password = getpass("Verify your password: ")
+    address = input("Address: ")
+    phone = input("Phone number: ")
