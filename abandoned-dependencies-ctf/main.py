085c73cc081eae261e459b48f994e79af2aea9f9
Cyrus Rice <cyrusric@amazon.com>
2021-06-16 22:34:10 -0700

    Added records pkg to abandoned dependencies ctf

--- abandoned-dependencies-ctf/main.py
+++ abandoned-dependencies-ctf/main.py
@@ -1,4 +1,5 @@
 from password.password import *
+from record.record import *
 if __name__ ==  "__main__":
     name = input ("Full name: ")
     dob = input ("Date of birth: ")
@@ -6,3 +7,9 @@ if __name__ ==  "__main__":
     re_password = getpass("Verify your password: ")
     address = input("Address: ")
     phone = input("Phone number: ")
+    
+    # using function from "good" record package that simply creates a 
+    # dictionary with all the above information
+    record = createRecord(name, dob, password, address, phone)
+    print("New record successfully created: \n")
+    print(record)
