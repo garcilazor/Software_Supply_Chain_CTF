ca080ca705be914c0ffaba64740343a4dc0b6984
jake-bickle <bickle.jake@gmail.com>
2021-06-27 15:03:26 -0700

    Debugging web server occurs over port 5000 now

--- compromise-dev-workflow-ctf/webserver/__main__.py
+++ compromise-dev-workflow-ctf/webserver/__main__.py
@@ -1,7 +1,7 @@
 from webserver.app import app
 
 def main():
-    app.run(host="0.0.0.0", debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=True, port=5000)
 
 if __name__ == "__main__":
     main()
