57e689c9fa736b59b9809bf782974a6444160e13
Cyrus Rice <cyrusric@amazon.com>
2021-07-13 18:04:32 -0700

    Fixed typo, removed Dockerfile in abandoned-dep ctf

--- abandoned-dependencies-ctf/main.py
+++ abandoned-dependencies-ctf/main.py
@@ -5,7 +5,7 @@ from emailpackage.emailpackage import *
 if __name__ ==  "__main__":
     name = input ("Full name: ")
     dob = input ("Date of birth: ")
-    password = getpass("Passowrd: ")
+    password = getpass("Password: ")
     re_password = getpass("Verify your password: ")
     address = input("Address: ")
     phone = input("Phone number: ")
