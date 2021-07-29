a0530ee6320a8e3fd4f3fec1b6d187e38a614c99
user <raga2@pdx.edu>
2021-07-28 22:25:15 -0700

    Ran flask command in background so script would stop hanging

--- compromise-dev-account-ctf/organization/run.sh
+++ compromise-dev-account-ctf/organization/run.sh
@@ -3,8 +3,7 @@ sleep 20
 while true
 do
 	pip install git+http://gitea/pallets/flask.git
-	flask run --host=$(hostname -I) -p 80
+	flask run --host=$(hostname -I) -p 80 &
 	sleep 10
-	kill -9 $(pgrep -f flask)
 	fuser -k 80/tcp
 done
