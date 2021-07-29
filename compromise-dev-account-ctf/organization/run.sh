dc90b10ca0a781f0a5c87796b0d5a44934c57eaa
user <raga2@pdx.edu>
2021-07-28 18:56:31 -0700

    Added package to organization container, and added fuser command to run.sh

--- compromise-dev-account-ctf/organization/run.sh
+++ compromise-dev-account-ctf/organization/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+sleep 20
 while true
 do
-	kill -9 $(pgrep -f flask)
 	pip install git+http://gitea/pallets/flask.git
 	flask run --host=$(hostname -I) -p 80
-	sleep 30
+	sleep 10
+	kill -9 $(pgrep -f flask)
+	fuser -k 80/tcp
 done
