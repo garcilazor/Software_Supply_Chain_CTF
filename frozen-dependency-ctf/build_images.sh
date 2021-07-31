fdba84433e1cda43a5594b5ce3a9f8a4716ae174
ectoglasses <ropun.tsd@gmail.com>
2021-07-31 16:45:56 -0700

    set up client and server images separately

---
+++ frozen-dependency-ctf/build_images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build client --tag fd-ctf-client
+docker build server --tag fd-ctf-server
---
+++ frozen-dependency-ctf/client/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "curl -F email=admin@me.com -F password=password --keepalive-time 6000 marsexp:8000/login" >> /root/.bashrc
