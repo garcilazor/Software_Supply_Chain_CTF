0443a6f045fc1993b7bf9fe036e44f11f82fe37a
Ramo <raga2@pdx.edu>
2021-08-20 15:39:35 -0700

    Updated docker-compose files

--- internal-dependency-ctf/buildAll.sh
+++ internal-dependency-ctf/buildAll.sh
@@ -1,5 +1,7 @@
 docker build attacker --tag sscsctf/levels:ctf-id-attacker
 docker build victim --tag sscsctf/levels:ctf-id-victim
+docker build pypiserver --tag sscsctf/levels:ctf-id-pypiserver
 
 #docker push sscsctf/levels:ctf-id-attacker
 #docker push sscsctf/levels:ctf-id-victim
+#docker push sscsctf/levels:ctf-id-pypiserver
