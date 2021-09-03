3fba9850621ca38f493fb60590990709ebbee7d6
Jaina Mellott <dev@jmellott.com>
2021-09-03 11:40:55 -0700

    Update build script for confused dependencies

--- confused-dependencies-ctf/build_all.sh
+++ confused-dependencies-ctf/build_all.sh
@@ -5,12 +5,10 @@ docker build . -f public.Dockerfile -t sscsctf/levels:ctf-cd-public
 docker build . -f private.Dockerfile -t sscsctf/levels:ctf-cd-private
 docker build . -f client.Dockerfile -t sscsctf/levels:ctf-cd-client
 docker build . -f tester.Dockerfile -t sscsctf/levels:ctf-cd-tester
-docker build . -f setup.Dockerfile -t sscsctf/levels:ctf-cd-setup
 
 # These will need to be pushed before the CTF is good to go live
-#docker push sscsctf/levels:ctf-cd-public
-#docker push sscsctf/levels:ctf-cd-private
-#docker push sscsctf/levels:ctf-cd-client
-#docker push sscsctf/levels:ctf-cd-tester
-#docker push sscsctf/levels:ctf-cd-setup
+docker push sscsctf/levels:ctf-cd-public
+docker push sscsctf/levels:ctf-cd-private
+docker push sscsctf/levels:ctf-cd-client
+docker push sscsctf/levels:ctf-cd-tester
 
